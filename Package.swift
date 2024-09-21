e8cd825d842d9e967ea87812e2ea2f88f0348d6d
Marc Schultz <lunij@users.noreply.github.com>
2024-09-21 20:50:50 +0200

    Bump minimum iOS version to 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Netbob",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "Netbob", targets: ["Netbob"])
